33995bb40e22666053a5de6a17378b0401865da8
Gabriel100904 <99267071+Gabriel100904@users.noreply.github.com>
2022-03-15 12:35:00 -0300

    Questão  1 prova batistela
    Alunos do grupo: Kauã Martins ,Gabriel Francisco , Gabriel Gouveia e Hiago Silveira , Julia Fontanive e Ana vitoria Wundervald

---
+++ prova.js
@@ -0,0 +1,46 @@
+let aluno1 = "Gabriel Neves"
+    let aluno2 = "Gabriel Gouveia"
+    let aluno3 = "Hiago Silveira"
+    let aluno4 = "Julia Fontaninive"
+    let aluno5 = "Ana Vitoria"
+    let aluno6 = "Kauã Martins"
+
+IsAtivo = true   
+IsAprovado = true
+IsReprovado = false
+IsRecusado = false
+IsAceito = true
+
+let numero1 = 2
+console.log (numero1)
+let numero2 = 5
+console.log (numero2)
+let numero3 = 10
+console.log (numero3)
+let numero4 = 15
+console.log(numero4)
+let numero5 = 20
+console.log(numero5)
+
+var salario1 = 3500
+console.log (salario1)
+var salario2 = 4500
+console.log (salario2)
+var salario3 = 2000
+console.log (salario3)
+var salario4 = 2500
+console.log (salario4)
+var salario5 = 5000
+console.log (salario5)
+
+
+const PI = 3.14
+console.log(PI);
+const AM = 2.11
+console.log(AM);
+const PM = 4.50
+console.log(PM);
+const NM = 5.40
+console.log(NM);
+const QM = 18.99
+console.log(QM);
