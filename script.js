1ba973f30c597bf39e7f47c1f6bc068fa6a6a2cb
Gabriel100904 <99267071+Gabriel100904@users.noreply.github.com>
2022-03-29 12:46:16 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,85 @@
+let q1 = document.querySelector('#q1')
+
+let verResultado = document.querySelector('#verResultado')
+verResultado.addEventListener('click', verificar)
+ 
+function verificar(){
+    if (q1.value == "Rio de Janeiro"){
+
+    }
+    else if (q1.value == "Minas Gerais"){
+
+    }
+
+    if (q2.value == "Ferruccio Lamborghini"){
+
+    }
+    else if (q2.value == "Tonino Lamborghini"){
+
+    }
+
+    if (q3.value == "1914"){
+
+    }
+    else if (q3.value == "1939"){
+
+    }
+
+    if (q3.value == "Ferdinand Porshe"){
+
+    }
+    else if (q3.value == "Louise Piëch "){
+
+    }
+
+    
+}
+
+if (q4.value == "Ferdinand Porsche"){
+
+}
+else if (q4.value == "Louise Piëch"){
+
+}
+
+if (q5.value == "Tom Holland"){
+
+}
+else if (q5.value == "Chris Pratt"){
+
+}
+
+if (q6.value == "Magnavox Odyssey"){
+
+}
+else if (q6.value == "Atari"){
+
+}
+   
+if (q7.value == "Martin Cooper"){
+
+}
+else if (q7.value == "Ronald Wayne"){
+
+}
+
+if (q8.value == "Henry Ford"){
+
+}
+else if (q8.value == "Harrison Ford"){
+
+}
+
+if (q9.value == "Martin Goodman"){
+
+}
+else if (q9.value == "Stan Lee"){
+
+}
+
+if (q10.value == "Malcolm Wheeler-Nicholson"){
+
+}
+else if (q10.value == "Henry Cavil"){
+
+}
