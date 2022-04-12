aca94ca4d9940d5c43507d282dd14db65d965aeb
Gabriel100904 <99267071+Gabriel100904@users.noreply.github.com>
2022-04-12 12:38:29 -0300

    Create case.js

---
+++ case.js
@@ -0,0 +1,18 @@
+let opcao = "uva"
+
+switch (key) {
+    case value:
+        console.log('Opcção selecionada é Uva')
+        break;
+    case 'laranja':
+        console.log('Opção selecionada é laranja')
+        break;
+        case 'Maça':
+            console.log('Opção selecionada é Maça')
+            break;
+        default:
+             console.log ('Opção invalida!')
+             break;
+    }
+
+
